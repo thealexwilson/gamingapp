8fab234ef145d4b7c9ea62927bb411fc366849a1
Alex Wilson <awilson@g2crowd.com>
2019-03-02 17:12:42 -0600

    Branding and default layout
    Established some branding colors, added color atomic, position atomic,
    added banner component, setup default layout, with new body container

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  # FIXME: shouldnt have to call out default - i think application.html.slim should be default, but application.html.erb is whats defaulting right now..
+  layout 'default'
+
+  def index
+  end
 end
